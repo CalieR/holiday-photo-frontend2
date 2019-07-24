9b043ad2415fcd1cb6f525a43a4b77c04bdc1fb4
CalieR <calielrushton@gmail.com>
2019-07-24 13:57:25 +0100

    added a sign in route

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home page.</h2>
+      <h4>Some generic stuff here and option to sign up </h4>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Signin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Signin() {
+  return <h2>Signin form</h2>;
+}
+
+export default Signin;
